d4b43a33b9fdebe29c5327baaa5414794d56f957
Jake <jmaxlee2002@gmail.com>
2025-08-07 23:46:03 -0400

    Added 6 kyu codewars solution: Sums of Parts

---
+++ 6kyu/sumsOfParts.js
@@ -0,0 +1,26 @@
+/*Let us consider this example (array written in general format):
+
+ls = [0, 1, 3, 6, 10]
+
+Its following parts:
+
+ls = [0, 1, 3, 6, 10]
+ls = [1, 3, 6, 10]
+ls = [3, 6, 10]
+ls = [6, 10]
+ls = [10]
+ls = []
+The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
+The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.
+*/
+
+function partsSums(ls) {
+  let reduce = ls.reduce((acc,c) => acc+c,0)
+  let parts_sums = [reduce]
+  for (x of ls){
+    reduce -= x
+    parts_sums.push(reduce)
+    }
+  return parts_sums
+}
